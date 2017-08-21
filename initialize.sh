670b7c4d2bd284b9387f8e0d7d6b09a9fcd7166f
zrone <xujining2008@126.com>
2017-08-22 01:01:00 +0800

    update Dockerfile

--- initialize.sh
+++ initialize.sh
@@ -1,24 +1,32 @@
 #!/bin/bash
 #
 apt-get update
-apt-get install -y libfreetype6-dev libjpeg62-turbo-dev libmcrypt-dev libpng12-dev libxml2-dev gettext libicu-dev libcurl3-openssl-dev libssl-dev ImageMagick libmagick++-dev
-docker-php-ext-install gd mbstring iconv shmop soap sockets zip mcrypt gettext pcntl intl
+apt-get install -y libfreetype6-dev libjpeg62-turbo-dev libmcrypt-dev libpng12-dev libxml2-dev gettext libicu-dev libcurl3-openssl-dev libssl-dev ImageMagick libmagick++-dev wget
+docker-php-ext-install -j$(nproc) iconv mcrypt
+docker-php-ext-install mbstring shmop soap sockets zip gettext pcntl intl
+docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
+docker-php-ext-install -j$(nproc) gd
 #
-pecl install igbinary
-pecl install swoole
+pecl install igbinary-2.0.1
+docker-php-ext-enable igbinary
+pecl install swoole-1.9.18
+docker-php-ext-enable swoole
 #
 expect -c "
-spawn /usr/local/bin/pecl install redis
+spawn /usr/local/bin/pecl install redis-3.1.3
 expect \"enable igbinary serializer support\? \[no\] \:\"
 send \"yes\r\"
 interact
 "
+docker-php-ext-enable redis
 #
-pecl install mongodb
-pecl install imagick
+pecl install mongodb-1.2.9
+docker-php-ext-enable mongodb
+pecl install imagick-3.4.3
+docker-php-ext-enable imagick
 #
 expect -c "
-spawn /usr/local/bin/pecl install solr
+spawn /usr/local/bin/pecl install solr-2.4.0
 expect \"Enable Solr Debugging \(Compiles solr in debug mode\) \[no\] \:\"
 send \"no\r\"
 expect \"libcURL install prefix \[\/usr\] \:\"
@@ -28,3 +36,4 @@ send \"\/usr\r\"
 interact
 "
 #
+docker-php-ext-enable solr
